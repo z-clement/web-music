4a7046544ef7d2749762eebc4fa5c6c3e89b364c
Zac C <zclement@gmail.com>
2024-01-26 15:08:43 -0500

    Initial commit

---
+++ api/lastfm-api.py
@@ -0,0 +1,2 @@
+# TODO code to interact with the last.fm api
+# https://www.last.fm/api/intro
---
+++ api/spotify-api.py
@@ -0,0 +1,2 @@
+# TODO functions to interact with the spotify api
+# https://developer.spotify.com/documentation/web-api
